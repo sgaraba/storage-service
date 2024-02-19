3be52159719dc9e1943ec4c185d605915fd1ef61
Sergiu Garaba <sergiu.garaba@esempla.com>
2024-02-19 12:03:52 +0200

    Review changesets

--- src/main/webapp/app/admin/admin.routes.ts
+++ src/main/webapp/app/admin/admin.routes.ts
@@ -13,24 +13,9 @@ const routes: Routes = [
     title: 'global.menu.admin.apidocs',
   },
   {
-    path: 'configuration',
-    loadComponent: () => import('./configuration/configuration.component'),
-    title: 'configuration.title',
-  },
-  {
-    path: 'health',
-    loadComponent: () => import('./health/health.component'),
-    title: 'health.title',
-  },
-  {
     path: 'logs',
     loadComponent: () => import('./logs/logs.component'),
     title: 'logs.title',
-  },
-  {
-    path: 'metrics',
-    loadComponent: () => import('./metrics/metrics.component'),
-    title: 'metrics.title',
   },
   /* jhipster-needle-add-admin-route - JHipster will add admin routes here */
 ];
