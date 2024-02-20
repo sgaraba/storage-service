7a9ac0bc2b3d7b1e013e3dc34dc32fd65b8fd2a4
nichita <staffordtony03@gmail.com>
2024-02-20 21:25:59 +0200

    added lang + UI changes

--- src/main/webapp/app/admin/reservation-space/list/list.component.ts
+++ src/main/webapp/app/admin/reservation-space/list/list.component.ts
@@ -4,6 +4,8 @@ import { AccountService } from '../../../core/auth/account.service';
 import { Account } from 'app/core/auth/account.model';
 import { ItemCountComponent } from '../../../shared/pagination';
 import SharedModule from '../../../shared/shared.module';
+import { ReservationSpaceService } from '../service/reservation.space.service';
+import { ReservationDTO } from '../../../entities/reservation/reservation.dto';
 
 @Component({
   selector: 'jhi-list',
@@ -14,12 +16,29 @@ import SharedModule from '../../../shared/shared.module';
 })
 export class ListComponent implements OnInit {
   currentAccount: Account | null = null;
+  reservations: ReservationDTO[] = [];
+  pagedReservations: ReservationDTO[] = [];
+
+  page!: number;
+  totalItems: number = 0;
+  itemsPerPage: number = 10;
+
   constructor(
     private accountService: AccountService,
     private alertService: AlertService,
+    private reservationSpaceService: ReservationSpaceService
   ) { }
 
   ngOnInit(): void {
     this.accountService.identity().subscribe(account => (this.currentAccount = account));
+    this.reservations = this.reservationSpaceService.get_reservationSpace_info();
+    this.totalItems = this.reservations.length;
+  }
+
+  // pagination
+  updatePage(): void {
+    const startIndex: number = (this.page - 1) * this.itemsPerPage;
+    const endIndex: number = Math.min(startIndex + this.itemsPerPage - 1, this.reservations.length - 1);
+    this.pagedReservations = this.reservations.slice(startIndex, endIndex + 1);
   }
 }
