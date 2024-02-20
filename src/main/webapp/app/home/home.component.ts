33678cde68eed615ea60278fedf2681bf646556e
nichita <staffordtony03@gmail.com>
2024-02-20 19:44:52 +0200

    deleted component used-space

--- src/main/webapp/app/home/home.component.ts
+++ src/main/webapp/app/home/home.component.ts
@@ -6,14 +6,13 @@ import { takeUntil } from 'rxjs/operators';
 import SharedModule from 'app/shared/shared.module';
 import { AccountService } from 'app/core/auth/account.service';
 import { Account } from 'app/core/auth/account.model';
-import { UsedSpaceComponent } from './used-space/used-space.component';
 
 @Component({
   standalone: true,
   selector: 'jhi-home',
   templateUrl: './home.component.html',
   styleUrl: './home.component.scss',
-  imports: [SharedModule, RouterModule, UsedSpaceComponent],
+  imports: [SharedModule, RouterModule],
 })
 
 export default class HomeComponent implements OnInit, OnDestroy {
