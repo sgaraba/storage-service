9899389cba834e77acaf72e67f35869d1f58bd9e
nichita <staffordtony03@gmail.com>
2024-02-21 22:15:05 +0200

    added component detail

--- src/main/webapp/app/admin/reservation-space/change-total-size/change-total-size.component.ts
+++ src/main/webapp/app/admin/reservation-space/change-total-size/change-total-size.component.ts
@@ -1,11 +1,12 @@
 import {Component} from '@angular/core';
 import SharedModule from '../../../shared/shared.module';
 import { ReactiveFormsModule } from '@angular/forms';
+import { RouterLink } from '@angular/router';
 
 @Component({
   selector: 'jhi-change-total-size',
   standalone: true,
-  imports: [SharedModule, ReactiveFormsModule],
+  imports: [SharedModule, ReactiveFormsModule, RouterLink],
   templateUrl: './change-total-size.component.html',
 })
 export class ChangeTotalSizeComponent {
--- src/main/webapp/app/admin/reservation-space/reservation-space.route.ts
+++ src/main/webapp/app/admin/reservation-space/reservation-space.route.ts
@@ -2,6 +2,7 @@ import { Component } from '@angular/core';
 import { Routes } from '@angular/router';
 import { ListComponent } from './list/list.component';
 import { ChangeTotalSizeComponent } from './change-total-size/change-total-size.component';
+import { ReservationSpaceDetailComponent } from './detail/reservation-space-detail.component';
 
 const ReservationSpaceRoute: Routes = [
   {
@@ -11,6 +12,10 @@ const ReservationSpaceRoute: Routes = [
   {
     path: 'change-total-size',
     component: ChangeTotalSizeComponent,
+  },
+  {
+    path: 'detail',
+    component: ReservationSpaceDetailComponent,
   }
 ];
 
