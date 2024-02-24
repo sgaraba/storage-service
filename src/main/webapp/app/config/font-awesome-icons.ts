8ea79971e9345d0b94c9189756329403a2d67c36
Nichita <staffordtony03@gmail.com>
2024-02-24 12:31:52 +0200

    localization + changed field names

--- src/main/webapp/app/config/font-awesome-icons.ts
+++ src/main/webapp/app/config/font-awesome-icons.ts
@@ -38,6 +38,7 @@ import {
   faUsersCog,
   faWrench,
   faDownload,
+  faFileAlt,
   // jhipster-needle-add-icon-import
 } from '@fortawesome/free-solid-svg-icons';
 
@@ -81,5 +82,6 @@ export const fontAwesomeIcons = [
   faUsersCog,
   faWrench,
   faDownload,
+  faFileAlt,
   // jhipster-needle-add-icon-import
 ];
