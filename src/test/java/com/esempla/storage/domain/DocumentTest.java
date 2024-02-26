3676ae276c025af91fad73e3c17535eba6069922
Sergiu Garaba <sergiu.garaba@esempla.com>
2024-02-26 14:15:01 +0200

    Add document entity

---
+++ src/test/java/com/esempla/storage/domain/DocumentTest.java
@@ -0,0 +1,24 @@
+package com.esempla.storage.domain;
+
+import static com.esempla.storage.domain.DocumentTestSamples.*;
+import static org.assertj.core.api.Assertions.assertThat;
+
+import com.esempla.storage.web.rest.TestUtil;
+import org.junit.jupiter.api.Test;
+
+class DocumentTest {
+
+    @Test
+    void equalsVerifier() throws Exception {
+        TestUtil.equalsVerifier(Document.class);
+        Document document1 = getDocumentSample1();
+        Document document2 = new Document();
+        assertThat(document1).isNotEqualTo(document2);
+
+        document2.setId(document1.getId());
+        assertThat(document1).isEqualTo(document2);
+
+        document2 = getDocumentSample2();
+        assertThat(document1).isNotEqualTo(document2);
+    }
+}
