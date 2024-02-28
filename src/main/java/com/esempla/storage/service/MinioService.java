f3280fee360122d035fbe4729008b527c43db09d
Ruslan Regush <ruslan.regus@isa.utm.md>
2024-02-28 15:39:34 +0200

    minio configuration , Not Work

--- src/main/java/com/esempla/storage/service/MinioService.java
+++ src/main/java/com/esempla/storage/service/MinioService.java
@@ -1,6 +1,10 @@
 package com.esempla.storage.service;
 
+import io.minio.BucketExistsArgs;
+import io.minio.MakeBucketArgs;
 import io.minio.MinioClient;
+import io.minio.errors.ErrorResponseException;
+import io.minio.errors.MinioException;
 import org.springframework.stereotype.Service;
 
 @Service
@@ -12,5 +16,16 @@ public class MinioService {
         this.minioClient = minioClient;
     }
 
-
+    public void createBucket(String bucketName) throws MinioException {
+        try {
+            boolean bucketExists = minioClient.bucketExists(BucketExistsArgs.builder().bucket(bucketName).build());
+            if (!bucketExists) {
+                minioClient.makeBucket(MakeBucketArgs.builder().bucket(bucketName).build());
+            }
+        } catch (ErrorResponseException e) {
+            throw new MinioException("Error creating bucket: " + bucketName + e);
+        } catch (Exception e) {
+            throw new MinioException("Unexpected error occurred while creating bucket: " + bucketName + e);
+        }
+    }
 }
