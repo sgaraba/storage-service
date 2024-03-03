461fe0f666ff99711eef75201b0646fac40b7cc1
Dubciuc <damian.dubciuc@iis.utm.md>
2024-03-03 03:32:44 +0200

    Added sidebar

--- src/main/webapp/app/app.routes.ts
+++ src/main/webapp/app/app.routes.ts
@@ -7,6 +7,7 @@ import {errorRoute} from './layouts/error/error.route';
 import HomeComponent from './home/home.component';
 import NavbarComponent from './layouts/navbar/navbar.component';
 import LoginComponent from './login/login.component';
+import { SidebarComponent } from './layouts/sidebar/sidebar.component';
 
 const routes: Routes = [
   {
@@ -18,6 +19,11 @@ const routes: Routes = [
     path: '',
     component: NavbarComponent,
     outlet: 'navbar',
+  },
+  {
+    path: '',
+    component: SidebarComponent,
+    outlet: 'sidebar',
   },
   {
     path: 'admin',
