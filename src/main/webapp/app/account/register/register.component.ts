c823e2be860ac450d0c876d931dbed96363a0b2f
Dubciuc <damian.dubciuc@iis.utm.md>
2024-03-20 12:04:35 +0200

    Fixed bugs

--- src/main/webapp/app/account/register/register.component.ts
+++ src/main/webapp/app/account/register/register.component.ts
@@ -44,7 +44,7 @@ export default class RegisterComponent implements AfterViewInit {
       nonNullable: true,
       validators: [Validators.required, Validators.minLength(4), Validators.maxLength(50)],
     }),
-    reservedSize: new FormControl('', {
+    rezervationSize: new FormControl('', {
       nonNullable: true,
       validators: [Validators.required],
     }),
@@ -81,9 +81,9 @@ export default class RegisterComponent implements AfterViewInit {
     if (password !== confirmPassword) {
       this.doNotMatch = true;
     } else {
-      const { login, email, reservedSize } = this.registerForm.getRawValue();
+      const { login, email, rezervationSize } = this.registerForm.getRawValue();
       this.registerService
-        .save({ login, email, password, reservedSize, langKey: this.translateService.currentLang })
+        .save({ login, email, password, rezervationSize, langKey: this.translateService.currentLang })
         .subscribe({ next: () => (this.success = true), error: response => this.processError(response) });
     }
   }
