26772d4975f4779567fe4f536834c27979e2b7be
Dubciuc <damian.dubciuc@iis.utm.md>
2024-03-20 19:32:31 +0200

    users and admin home page

--- src/main/webapp/app/home/home.component.ts
+++ src/main/webapp/app/home/home.component.ts
@@ -6,6 +6,8 @@ import { takeUntil } from 'rxjs/operators';
 import SharedModule from 'app/shared/shared.module';
 import { AccountService } from 'app/core/auth/account.service';
 import { Account } from 'app/core/auth/account.model';
+import { Authority } from 'app/config/authority.constants';
+
 
 @Component({
   standalone: true,
