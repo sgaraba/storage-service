d367cd5d6d37dce6776bd499dcdc99397db312e7
nichita <staffordtony03@gmail.com>
2024-03-25 16:40:36 +0200

    fix translate RO error

--- src/main/webapp/app/home/dashboard/admin/admin-dashboard.component.ts
+++ src/main/webapp/app/home/dashboard/admin/admin-dashboard.component.ts
@@ -3,6 +3,9 @@ import SharedModule from 'app/shared/shared.module';
 import { DetailsComponent } from './details/details.component';
 import { StatisticsDocumentsComponent } from './statistics-documents/statistics-documents.component';
 import { RecentDocumentsComponent } from './recent-documents/recent-documents.component';
+import localeRo from '@angular/common/locales/ro';
+import { registerLocaleData } from '@angular/common';
+registerLocaleData(localeRo);
 
 @Component({
   standalone: true,
