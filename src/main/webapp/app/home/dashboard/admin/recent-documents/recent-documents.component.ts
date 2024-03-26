2e5fbdeac27450d4d77e07d532709deb572d7fef
nichita <staffordtony03@gmail.com>
2024-03-26 15:51:02 +0200

    component RecentDocuments -> get files by request

--- src/main/webapp/app/home/dashboard/admin/recent-documents/recent-documents.component.ts
+++ src/main/webapp/app/home/dashboard/admin/recent-documents/recent-documents.component.ts
@@ -1,4 +1,4 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
 import { IconProp } from '@fortawesome/fontawesome-svg-core';
 import { FileModel } from 'app/entities/files/file.model';
 import { FileService } from 'app/entities/files/service/file.service';
@@ -16,22 +16,17 @@ import { RecentDocumentsService } from './service/recent-documents.service';
   styleUrls: ['./recent-documents.component.scss'],
   imports: [SharedModule, GetFileIcon],
 })
-export class RecentDocumentsComponent {
+export class RecentDocumentsComponent implements OnInit {
   files: FileModel[] | null = null;
 
   constructor(
     private fileService: FileService,
     private modalService: NgbModal,
     private recentDocumentsService: RecentDocumentsService
-  ) {
-    this.recentDocumentsService.getRecentFiles().subscribe({
-      next: (files: FileModel[]) => {
-        this.files = files;
-      },
-      error: (error) => {
-        console.error('Error fetching recent files:', error);
-      }
-    });
+  ) {}
+
+  ngOnInit(): void {
+    this.loadAll();
   }
 
   call_download_fileService(id: number): void {
@@ -47,7 +42,22 @@ export class RecentDocumentsComponent {
     const modalRef = this.modalService.open(DeleteComponent);
     modalRef.componentInstance.fileID = fileID;
 
-    modalRef.closed.subscribe();
+    modalRef.closed.subscribe(reason => {
+      if (reason === 'deleted') {
+        this.loadAll();
+      }
+    });
+  }
+
+  loadAll(){
+    this.recentDocumentsService.getRecentFiles().subscribe({
+      next: (files: FileModel[]) => {
+        this.files = files;
+      },
+      error: (error) => {
+        console.error('Error fetching recent files:', error);
+      }
+    });
   }
 
   private base64Decode(base64String: string): Uint8Array {
--- src/main/webapp/app/home/dashboard/admin/recent-documents/service/recent-documents.service.ts
+++ src/main/webapp/app/home/dashboard/admin/recent-documents/service/recent-documents.service.ts
@@ -1,24 +1,17 @@
 import { Injectable } from '@angular/core';
-import { HttpClient, HttpResponse } from '@angular/common/http';
-import { Observable, of } from 'rxjs';
+import { HttpClient } from '@angular/common/http';
+import { Observable } from 'rxjs';
 import { FileModel } from 'app/entities/files/file.model';
+import { ApplicationConfigService } from 'app/core/config/application-config.service';
 
 @Injectable({ providedIn: 'root' })
 export class RecentDocumentsService {
-  constructor(private http: HttpClient) {}
+  constructor(
+    private http: HttpClient,
+    private applicationConfigService: ApplicationConfigService
+  ) {}
 
   getRecentFiles(): Observable<FileModel[]> {
-    const files: FileModel[] = [];
-    for (let i = 0; i < 10; i++) {
-      const file: FileModel = {
-        id: i + 1,
-        name: `File ${i + 1}.csv`,
-        mimeType: 'application/pdf',
-        createdBy: 'John Doe',
-        createdDate: new Date(),
-      };
-      files.push(file);
-    }
-    return of(files);
+    return this.http.get<FileModel[]>(this.applicationConfigService.getEndpointFor('/api/dashboard/recent-files'));
   }
 }
